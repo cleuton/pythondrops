b604eb264e7b26fd0d92fef76135d1dac921737c
Cleuton Sampaio <cleuton.sampaio@gmail.comm>
2019-01-03 15:20:01 -0200

    Lição1

--- curso/maze/maze.py
+++ curso/maze/maze.py
@@ -1,7 +1,10 @@
 from model.labirinto import Labirinto
 from solver import Solver
+import sys
+linhas = int(sys.argv[1]) if len(sys.argv)>1 else 10
+colunas = int(sys.argv[2]) if len(sys.argv)>2 else 10
 
-labirinto = Labirinto(5,50)
+labirinto = Labirinto(linhas,colunas)
 print(labirinto)
 
 solver = Solver()
